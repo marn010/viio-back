21a56300c70a9be5b3f17e34570d8c9dcf2b4a13
Miguel Rivera <mariveran@unal.edu.co>
2023-11-18 07:59:00 -0500

    change mysql model

--- models/mysql/product.js
+++ models/mysql/product.js
@@ -5,7 +5,7 @@ const DEFAULT_CONFIG = {
   host: 'localhost',
   user: 'root',
   port: 3306,
-  password: 'inuyasha010',
+  password: '',
   database: 'signup'
 }
 const connectionString = DEFAULT_CONFIG
